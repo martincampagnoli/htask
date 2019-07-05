937429ef2617c986fcbaa0c03896116884dc7264
Martin Campagnoli <mcampagnoli@canto.com>
2019-07-05 14:48:58 +0200

    Add star rating component and display functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'htask';
+  items: any[] = [];
+  starCount: 5;
+  filteredOptions: Observable<any[]>;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() {
+    let items$ = this.http.get('../assets/items.json');
+    items$.subscribe(
+                response => {
+                        this.items = response as [];
+                        this.items = this.items.map(x => ({ ...x, rating: 1 }));
+                      });
+  }
+
+  onRatingChanged(rating, item){
+    console.dir("need to reorder")
+    item.rating = rating;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { StarRatingComponent } from './star-rating/star-rating.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarRatingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
