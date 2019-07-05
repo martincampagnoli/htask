aa4e92a23304c2894ebed783dc1b5c96190e5047
Martin Campagnoli <mcampagnoli@canto.com>
2019-07-05 17:12:56 +0200

    Add random rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'htask';
+  rrOn = 'Start';
+  rrOff = 'Stop';
+  rr = false;
+  randomRatingString = this.rrOn;
   items: any[] = [];
+  subs: any;
+  randomRating$: Observable;
+
 
   constructor(private http: HttpClient){
-
   }
 
   ngOnInit() {
+    this.randomRating$ = new Observable(function subscribe(subscriber) {
+    const intervalId = setInterval(() => {
+      let randomIndex = Math.floor(Math.random() * 9) + 0;
+      let randomValue = Math.floor(Math.random() * 5) + 1
+      subscriber.next({randomIndex, randomValue});
+    }, 500);
+    return function unsubscribe() {
+      clearInterval(intervalId);
+      };
+    });
     let items$ = this.http.get('../assets/items.json');
     items$.subscribe(
                 response => {
@@ -28,11 +45,27 @@ export class AppComponent implements OnInit {
     this._filter();
   }
 
+  toogleRandomRating(){
+    if (!this.rr) {
+      this.randomRatingString = this.rrOff;
+      this.subs = this.randomRating$.subscribe(r =>{
+        this.onRatingChanged(r.randomValue, this.items[r.randomIndex]);
+      })
+    }
+    else {
+      this.randomRatingString = this.rrOn;
+      this.subs.unsubscribe();
+    }
+    this.rr = !this.rr;
+  }
+
   private _filter(){
     this.items.sort((a, b) => {
         if (a.rating < b.rating) return 1;
         else if (a.rating > b.rating) return -1;
         else return 0;
       });
-    }
+  }
+
+
 }
